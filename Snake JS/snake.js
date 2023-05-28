ce032c208f7c79d4b738edb2c9fffe832743c552
Thomas Palmen <121097637+thmaz@users.noreply.github.com>
2023-05-28 15:37:43 +0200

    Game over voorwaarden toegevoegd, update functie aangepast

--- Snake JS/snake.js
+++ Snake JS/snake.js
@@ -10,9 +10,18 @@ var context;
 var snakeX = blockSize * 5;
 var snakeY = blockSize * 5;
 
+var velocityX = 0;
+var velocityY = 0;
+
+var snakeBody = [];
+
 //appeltjes
 var foodX;
 var foodY;
+
+
+//game over regels
+var gameOver = false; 
 
 window.onload = function() {
     board = document.getElementById("board");
@@ -21,21 +30,74 @@ window.onload = function() {
     context = board.getContext("2d"); 
 
     placeFood();
-    update();
+    document.addEventListener("keyup", changeDirection);
+    //update();
+    setInterval(update, 1000/10); //update elke 100 ms
 }
 
 function update() {
+    if (gameOver) {
+        return;
+    }
+
     context.fillStyle="black";
     context.fillRect(0, 0, board.width, board.height);
 
     context.fillStyle="lime";
+    snakeX += velocityX * blockSize;
+    snakeY += velocityY * blockSize;
     context.fillRect(snakeX, snakeY, blockSize, blockSize);
+    for (let i = 0; i < snakeBody.length; i++){
+        context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+    }  //draws nieuwe snake body bij elke appel gegeten
+
+    //"game over" voorwaarden
+    if (snakeX < 0 || snakeX > cols*blockSize || snakeY < 0 || snakeY > rows*blockSize) {
+        gameOver = true;
+        alert("Game Over");
+    }
+    for (let i = 0; i < snakeBody.length; i++){
+        if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
+            gameOver = true;
+            alert("Game Over");
+        }
+    }
 
     context.fillStyle="red";
     context.fillRect(foodX, foodY, blockSize, blockSize);
+
+    if (snakeX == foodX && snakeY == foodY) {
+        snakeBody.push([foodX, foodY]);
+        placeFood();
+    }
+    for (let i = snakeBody.length-1; i > 0; i--){
+        snakeBody[i] = snakeBody[i-1];
+    } //laat snake bodyparts weten waar zij zich moeten positioneren om achter snake te blijven
+    if (snakeBody.length) {
+        snakeBody[0] = [snakeX, snakeY]; 
+    }
 }
 
 function placeFood() {
     foodX = Math.floor(Math.random() * cols) * blockSize;
     foodY = Math.floor(Math.random() * rows) * blockSize;
+}
+
+function changeDirection(e) {
+    if (e.code == "ArrowUp" && velocityY != 1) {
+        velocityX = 0;
+        velocityY = -1;
+    }
+    else if (e.code == "ArrowDown" && velocityY != -1) {
+        velocityX = 0;
+        velocityY = 1;
+    }
+    else if (e.code == "ArrowLeft" && velocityX != 1) {
+        velocityX = -1;
+        velocityY = 0;
+    }
+    else if (e.code == "ArrowRight" && velocityX != -1) {
+        velocityX = 1;
+        velocityY = 0;
+    } //function om snake beweging functioneel te maken
 }
